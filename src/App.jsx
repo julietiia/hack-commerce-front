4e6ddb73e114f072f128d662293e2a8db3d9f70e
AnaBaccaro <analaurabaccaro@gmail.com>
2023-11-28 10:05:23 -0300

    product route created

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Product from "./components/Product";
 import { Route, Routes } from "react-router-dom";
-
-
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      < Footer />
+      <Routes>
+        <Route>
+        <Route path="/" element={<Home/>} />
+        <Route path="/product/:id" element={<Product/>} />
+        </Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
+
 
 export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <>
+      <div>hola</div>
+  
+    </>
+  );
+}
+
+export default Product
