e3f9603ae5f26d8a8cfd698a98b6ee8b69368e96
vanina312B <134708728+vanina312B@users.noreply.github.com>
2023-11-28 10:04:23 -0300

    img & about us update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,8 +7,8 @@ function AboutUs() {
         <>
             <Navbar />
             <div className="container">
-                <div className="row mt-3">
-                    <div className="col-6">
+                <div className="row mt-4">
+                    <div className="col-6 border rounded pt-3">
                         <h1>What's apparat?</h1>
                         <p className="pt-3">
                             Lorem ipsum dolor sit amet consectetur adipisicing
@@ -21,11 +21,12 @@ function AboutUs() {
                             necessitatibus consequuntur aliquid obcaecati.
                             Praesentium ad sequi, et laborum aspernatur dolore
                             ab.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore necessitatibus tempora debitis ea beatae aspernatur sit, quasi quae nihil corrupti. Illum eos corporis modi. Pariatur ipsum tempore tenetur quasi provident!
                         </p>
                     </div>
-                    <div className="col-6 bg-black">
+                    <div className="col-6 bg-black d-flex justify-content-center rounded">
                         <img
-                            src="/src/assets/apparat-logo-black.png"
+                            src="/src/assets/apparat_white-navbar-logo.svg"
                             alt=""
                             className="logo"
                         />
@@ -33,26 +34,14 @@ function AboutUs() {
                 </div>
                 </div>
                 <div className="container">
-                <h2>Technologies & Tools</h2>
-                  <div className="row">
-                <div className="col-2">
-                  <img src="" alt="" className="border" />
-                </div>
-                <div className="col-2">
-                  <img src="" alt="" className="border" />
-                </div>
-                <div className="col-2">
-                  <img src="" alt="" className="border" />
-                </div>
-                <div className="col-2">
-                  <img src="" alt="" className="border" />
-                </div>
-                <div className="col-2">
-                  <img src="" alt="" className="border" />
-                </div>
-                <div className="col-2 border">
-                  <img src="" alt="" className="border" />
-                </div>
+                <h2 className="d-flex justify-content-center mt-4">Technologies & Tools</h2>
+                  <div className="row border rounded tech-row mt-3 d- ">
+                    <div className="col">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum atque doloribus eligendi dolor. Eveniet alias quia accusantium debitis aliquam ex fuga, dolores qui, placeat eum et recusandae fugit nihil. Dignissimos.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga molestiae harum nisi aperiam, repudiandae magni repellendus tempora in, modi ex, nesciunt maxime ad odit quibusdam cum at nobis laudantium unde.</p>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit ducimus tempora sequi repudiandae excepturi illo aspernatur, dignissimos praesentium suscipit dolor totam, ipsum incidunt consectetur, sed eius laudantium possimus cum?</p>
+                    </div>
+                
                 </div>
                 </div>
                 
