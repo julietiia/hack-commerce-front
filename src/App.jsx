f62d9aef5b27b06fdeda6ab4746004499f63ab9b
helmdyck <helmuth.dyck@gmail.com>
2023-11-28 10:18:33 -0300

    Ruta Chekout Creada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Product from "./components/Product";
 import { Route, Routes } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
+import CheckOut from "./pages/CheckOut"
 
 function App() {
     return (
@@ -16,6 +16,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/checkout" element={<CheckOut />} />
             </Routes>
         </>
     );
