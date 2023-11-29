206098e9612becab1698e8173c4f7bf301753ff0
vanina312B <134708728+vanina312B@users.noreply.github.com>
2023-11-29 12:34:13 -0300

    category chair

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,7 +12,7 @@ function Category() {
         <div className="col">
          <div className="image-container">
           <img src="../src/assets/alexa_chair.png" alt="image1" className='img-chair img1'  />
-          <img src="../src/assets/alexia_chair_ls.webp" alt="image2" className='img-chair img2' />
+          <img src="../src/assets/alexia_chair_ls.webp" alt="image2" className='img-chair top-img' />
           <small>Alexa chair</small>
           <small>250 $</small>
         </div>
@@ -20,7 +20,7 @@ function Category() {
         <div className="col">
           <div className="image-container">
           <img src="../src/assets/charlotte_chair.png" alt="image1" className='img-chair img1' />
-          <img src="../src/assets/charlotte-chair-ls.webp" alt="image2" className='img-chair img2' />
+          <img src="../src/assets/charlotte-chair-ls.webp" alt="image2" className='img-chair top-img' />
           <small>Charlotte chair</small>
           <small>999 $</small>
           </div>
@@ -28,7 +28,7 @@ function Category() {
         <div className="col">
           <div className="image-container">
           <img src="../src/assets/wallie_chair.png" alt="image1" className='img-chair img1'/>
-          <img src="../src/assets/wallie_chair_ls.webp" alt="image1" className='img-chair img2'/>
+          <img src="../src/assets/wallie_chair_ls.webp" alt="image1" className='img-chair top-img'/>
           <small>Wallie chair</small>
           <small>599 $</small>
         </div>
@@ -36,7 +36,7 @@ function Category() {
         <div className="col">
           <div className="image-container">
           <img src="../src/assets/liam_chair.png" alt="image1" className='img-chair img1'/>
-          <img src="../src/assets/liam_chair_ls.webp" alt="image1" className='img-chair img2'/>
+          <img src="../src/assets/liam_chair_ls.webp" alt="image1" className='img-chair top-img'/>
           <small>Liam chair</small>
           <small> 499$</small>
         </div>
