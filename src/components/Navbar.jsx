062b144b47ceda80f618219916b86546ab119d30
julietiia <111154000+julietiia@users.noreply.github.com>
2023-12-01 11:23:19 -0300

    Navbar responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import OffCanvasSignIn from "./OffCanvasSignIn";
-import "./css/App.css"
+import "./css/App.css";
+import OffCanvasNavbarMenu from "./OffCanvasNavbarMenu";
 
 function NavbarApparat() {
   const [showCart, setShowCart] = useState(false);
@@ -18,85 +19,103 @@ function NavbarApparat() {
   const handleShowSignIn = (event) => setShowSignIn(true);
   const handleCloseCart = (event) => setShowCart(false);
   const handleCloseSignIn = (event) => setShowSignIn(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const handleToggleMenu = () => setShowMenu(!showMenu);
+  const handleCloseMenu = () => setShowMenu(false);
+
   const navigate = useNavigate();
 
   return (
     <>
-    <div className="parent-navbar-container p-5">
-      <Navbar expand="lg" className="myNavbar higher-z-index">
-        <Container>
-          <Navbar.Brand className="" href="/">
-            <div onClick={() => navigate("/")} className="apparat-logo">
-              <img
-                className="apparat-logo-navbar m-0"
-                src="src/assets/logo/apparat+black-navbar-logo.png"
-                alt="Apparat logo"
-              />
-            </div>
-          </Navbar.Brand>
-          <Navbar.Toggle  aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavLink className="nav-link custom-item" to="/">
-                home
-              </NavLink>
-              <NavLink className="nav-link custom-item" to="#">
-                shop
-              </NavLink>
-              <NavLink
-                className="nav-link custom-item text-black"
-                to="/about-this-project"
-              >
-                about this project
-              </NavLink>
-              <NavDropdown
-                className="text-black"
-                title="categories"
-                id="basic-nav-dropdown"
-              >
-                <NavLink className="dropdown-item" to="/category/chairs">
-                  chairs
+      <div className="parent-navbar-container p-4">
+        <Navbar expand="lg" className="navbar higher-z-index">
+          <Container>
+            <Navbar.Brand className="" href="/">
+              <div onClick={() => navigate("/")} className="apparat-logo">
+                <img
+                  className="apparat-logo-navbar m-0"
+                  src="src/assets/logo/apparat+black-navbar-logo.png"
+                  alt="Apparat logo"
+                />
+              </div>
+            </Navbar.Brand>
+            {/* <Navbar.Collapse id="basic-navbar-nav" className={showMenu ? "show" : ""}>  */}
+              <Nav className="me-auto d-sm-none d-lg-flex collapse-responsive">
+                <NavLink className="nav-link custom-item" to="/">
+                  home
                 </NavLink>
-                <NavLink className="dropdown-item" to="/category/sofas">
-                  sofas
+                <NavLink className="nav-link custom-item" to="#">
+                  shop
                 </NavLink>
-                <NavLink className="dropdown-item" to="/category/tables">
-                  tables
+                <NavLink
+                  className="nav-link custom-item text-black"
+                  to="/about-this-project"
+                >
+                  about this project
                 </NavLink>
-                <NavLink className="dropdown-item" to="/category/lightning">
-                  lightning
-                </NavLink>
-                <NavLink className="dropdown-item" to="/category/storage">
-                  storage&organization
-                </NavLink>
-                <NavLink className="dropdown-item" to="/category/deco">
-                  deco
-                </NavLink>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-          <div className="shop-profile">
-            <i className="bi bi-search lupita"></i>
+
+                <NavDropdown
+                  className="text-black"
+                  title="categories"
+                  id="basic-nav-dropdown"
+                >
+                  <NavLink className="dropdown-item" to="/category/chairs">
+                    chairs
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/category/sofas">
+                    sofas
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/category/tables">
+                    tables
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/category/lightning">
+                    lightning
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/category/storage">
+                    storage&organization
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/category/deco">
+                    deco
+                  </NavLink>
+                </NavDropdown>
+              </Nav>
+              <div className="shop-profile d-sm-none d-lg-flex">
+                <i className="bi bi-search lupita"></i>
+                <i
+                  onClick={(event) => handleShowCart(event)}
+                  className="bi bi-cart3 carrito-icon"
+                ></i>
+                <i
+                  onClick={(event) => handleShowSignIn(event)}
+                  className="bi bi-person-circle profile-icon"
+                ></i>
+              </div>
+            {/* </Navbar.Collapse> */}
+            <i
+              onClick={handleToggleMenu}
+              className="bi bi-list mobile-menu-icon d-lg-none"
+            ></i>
+
             <i
               onClick={(event) => handleShowCart(event)}
-              className="bi bi-cart3 carrito-icon"
+              className="bi bi-cart3 carrito-icon-responsive"
             ></i>
-            <i
-              onClick={(event) => handleShowSignIn(event)}
-              className="bi bi-person-circle profile-icon"
-            ></i>
-          </div>
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+            <OffCanvasNavbarMenu
+              showMenu={showMenu}
+              handleCloseMenu={() => setShowMenu(false)}
+            />
 
-          <OffCanvasShoppingCart
-            showCart={showCart}
-            handleCloseCart={() => setShowCart(false)}
-          />
-          <OffCanvasSignIn
-            showSignIn={showSignIn}
-            handleCloseSignIn={() => setShowSignIn(false)}
-          />
-        </Container>
-      </Navbar>
+            <OffCanvasShoppingCart
+              showCart={showCart}
+              handleCloseCart={() => setShowCart(false)}
+            />
+            <OffCanvasSignIn
+              showSignIn={showSignIn}
+              handleCloseSignIn={() => setShowSignIn(false)}
+            />
+          </Container>
+        </Navbar>
       </div>
     </>
   );
---
+++ src/components/OffCanvasNavbarMenu.jsx
@@ -0,0 +1,99 @@
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Navbar from "./Navbar";
+import { Nav, NavLink, NavDropdown } from "react-bootstrap";
+import "./css/App.css";
+import { useNavigate } from "react-router-dom";
+import OffCanvasSignIn from "./OffCanvasSignIn";
+import OffCanvasShoppingCart from "./OffCanvasShoppingCart";
+
+function OffCanvasNavbarMenu({
+  showMenu,
+  handleShowSignIn,
+  handleCloseMenu,
+  handleToggleMenu,
+  showCart,
+  handleCloseCart,
+  showSignIn,
+  handleCloseSignIn,
+   ...props
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Offcanvas
+        show={showMenu}
+        onHide={handleCloseMenu}
+        {...props}
+        placement={"end"}
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="me-auto">
+            <NavLink className="nav-link custom-item" to="/">
+              home
+            </NavLink>
+            <NavLink className="nav-link custom-item" to="#">
+              shop
+            </NavLink>
+            <NavLink
+              className="nav-link custom-item text-black"
+              to="/about-this-project"
+            >
+              about this project
+            </NavLink>
+
+            <NavDropdown
+              className="text-black"
+              title="categories"
+              id="basic-nav-dropdown"
+            >
+              <NavLink className="dropdown-item" to="/category/chairs">
+                chairs
+              </NavLink>
+              <NavLink className="dropdown-item" to="/category/sofas">
+                sofas
+              </NavLink>
+              <NavLink className="dropdown-item" to="/category/tables">
+                tables
+              </NavLink>
+              <NavLink className="dropdown-item" to="/category/lightning">
+                lightning
+              </NavLink>
+              <NavLink className="dropdown-item" to="/category/storage">
+                storage&organization
+              </NavLink>
+              <NavLink className="dropdown-item" to="/category/deco">
+                deco
+              </NavLink>
+            </NavDropdown>
+          </Nav>
+          <div className="shop-profile d-flex">
+            <i className="bi bi-search lupita d-block"></i>
+            <i
+              onClick={(event) => handleShowCart(event)}
+              className="bi bi-cart3 carrito-icon"
+            ></i>
+            <i
+              onClick={(event) => handleShowSignIn(event)}
+              className="bi bi-person-circle profile-icon d-block"
+            ></i>
+          </div>
+
+          <OffCanvasShoppingCart
+            showCart={showCart}
+            handleCloseCart={() => setShowCart(false)}
+          />
+          <OffCanvasSignIn
+            showSignIn={showSignIn}
+            handleCloseSignIn={() => setShowSignIn(false)}
+          />
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default OffCanvasNavbarMenu;
