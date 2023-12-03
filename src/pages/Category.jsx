ae57a7c6b1c7e6f3a5d405a57b7e042a7e46bad4
julietiia <111154000+julietiia@users.noreply.github.com>
2023-12-03 20:25:16 -0300

    Added button to category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,98 +5,96 @@ import "../components/css/Category.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { Carousel } from "react-bootstrap";
 import ProductCarousel from "../components/ProductCarousel";
+import AddToCartButton from "../components/AddToCartButton";
 
 function Category() {
-    const navigate = useNavigate();
-    const params = useParams();
-    const id = params.id;
-    const [category, setCategory] = useState();
-    const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id;
+  const [category, setCategory] = useState();
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const getCategory = async () => {
-            const response = await axios({
-                method: "get",
-                url: `${import.meta.env.VITE_PORT_URL}/category/${id}`,
-            });
-            setCategory(response.data.category);
-            setProducts(response.data.products);
-            console.log(category, products);
-        };
-        getCategory();
-    }, [id]);
-    // se rompe
+  useEffect(() => {
+    const getCategory = async () => {
+      const response = await axios({
+        method: "get",
+        url: `${import.meta.env.VITE_PORT_URL}/category/${id}`,
+      });
+      setCategory(response.data.category);
+      setProducts(response.data.products);
+    };
+    getCategory();
+  }, []);
+  // se rompe
 
-    return (
-        <>
-            {/* <p>{category.name}</p> */}
-            <div className="header">
-                <img
-                    src="../src/assets/img/banner-category-sillas.jpg"
-                    alt="image1"
-                    className="img-banner-category-chairs"
-                />
-            </div>
-            <div className="container">
-                <div className="row mb-5 mt-5">
-                    {products.map((product) => (
-                        <div key={product.id} className="col-6 col-lg-3">
-                            <div className="d-lg-none">
-                                <div className="image-container">
-                                    <Carousel >
-                                        {product.image.map((img, index) => (
-                                            <Carousel.Item
-                                                key={index}
-                                                onClick={() =>
-                                                    navigate(
-                                                        `/product/${product.id}`
-                                                    )
-                                                }
-                                            >
-                                                <img
-                                                    src={`${
-                                                        import.meta.env
-                                                            .VITE_IMAGES_URL
-                                                    }products/${img}`}
-                                                    alt={product.name}
-                                                    className="img-chair"
-                                                />
-                                            </Carousel.Item>
-                                        ))}
-                                    </Carousel>
-                                </div>
-                            </div>
-                            <div className="d-none d-lg-block">
-                                <div className="image-container">
-                                    <img
-                                        onClick={() =>
-                                            navigate(`/product/${product.id}`)
-                                        }
-                                        src={`${
-                                            import.meta.env.VITE_IMAGES_URL
-                                        }products/${product.image[1]}`}
-                                        alt={product.name}
-                                        className="img-chair img1"
-                                    />
-
-                                    <img
-                                        src={`${
-                                            import.meta.env.VITE_IMAGES_URL
-                                        }products/${product.image[0]}`}
-                                        alt={product.name}
-                                        className="img-chair top-img"
-                                    />
-                                </div>
-                            </div>
-
-                            <small>{product.name}</small>
-                            <small>{product.price}USD</small>
-                        </div>
+  return (
+    <>
+      <div className="header">
+        <img
+          src="../src/assets/img/banner-category-sillas.jpg"
+          alt="image1"
+          className="img-banner-category-chairs"
+        />
+      </div>
+      <h1 className="text-center mt-5">{category.name}</h1>
+      <div className="container">
+        <div className="row mb-5 mt-5">
+          {products.map((product) => (
+            <div key={product.id} className="col-6 col-lg-3">
+              <div className="d-lg-none">
+                <div className="image-container">
+                  <Carousel>
+                    {product.image.map((img, index) => (
+                      <Carousel.Item
+                        key={index}
+                        onClick={() => navigate(`/product/${product.id}`)}
+                      >
+                        <img
+                          src={`${
+                            import.meta.env.VITE_IMAGES_URL
+                          }products/${img}`}
+                          alt={product.name}
+                          className="img-chair"
+                        />
+                      </Carousel.Item>
                     ))}
+                  </Carousel>
                 </div>
+              </div>
+              <div className="d-none d-lg-block">
+                <div className="image-container">
+                  <img
+                    onClick={() => navigate(`/product/${product.id}`)}
+                    src={`${import.meta.env.VITE_IMAGES_URL}products/${
+                      product.image[1]
+                    }`}
+                    alt={product.name}
+                    className="img-chair img1"
+                  />
+
+                  <img
+                    src={`${import.meta.env.VITE_IMAGES_URL}products/${
+                      product.image[0]
+                    }`}
+                    alt={product.name}
+                    className="img-chair top-img"
+                  />
+                </div>
+              </div>
+
+              <div className="shop-product-info d-flex flex-column mb-4">
+                <p className="m-0">{product.name}</p>
+                <p id="price" className="mb-2 fw-light">
+                  {product.price}USD
+                </p>
+                <AddToCartButton />
+              </div>
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Category;
