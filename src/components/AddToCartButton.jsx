e4b15e89b3225ef321acf40bdd740ef1011f0d62
julietiia <111154000+julietiia@users.noreply.github.com>
2023-12-06 21:45:54 -0300

    Added toggleShow modal on addtocart btn

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cartSlice";
+import { toggleShowModal } from "../redux/pageSlice";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const AddToCartButton = ({ product, quantity }) => {
   const dispatch = useDispatch();
-
+  const showCart = useSelector((state) => state.page.showCart)
   const handleAddToCart = () => {
     dispatch(addToCart({ product, quantity }));
+    dispatch(toggleShowModal()); 
   };
 
   return (
