459fa1182d9fcdf22819dc078be1abc988f0a82c
helmdyck <helmuth.dyck@gmail.com>
2023-12-09 23:10:31 -0300

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,8 +18,8 @@ function Footer() {
 
   return (
     <footer>
-      <div className="bg-dark text-white">
-        <div onClick={() => navigate("/")} className="footer-logo-container">
+      <div className="bg-dark text-white pt-3">
+        <div onClick={() => navigate("/")} className="footer-logo-container pt-1">
           <img
             id="footerLogo"
             src="/src/assets/logo/apparat+white-navbar-logo.png"
@@ -69,7 +69,7 @@ function Footer() {
                           lighting
                         </li>
                         <li onClick={() => navigate("/category/storage")}>
-                          storage & organisation
+                          storage & organization
                         </li>
                         <li onClick={() => navigate("/category/deco")}>deco</li>
                       </ul>
@@ -154,7 +154,11 @@ function Footer() {
             </div>
             <div className="col-sm-12 col-md-3 text-center">
               <h4>let's keep in touch</h4>
-              <a className="mx-4" href="https://www.facebook.com/" target="blank">
+              <a
+                className="mx-4"
+                href="https://www.facebook.com/"
+                target="blank"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -166,7 +170,11 @@ function Footer() {
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                 </svg>
               </a>
-              <a className="mx-4" href="https://twitter.com/home" target="blank">
+              <a
+                className="mx-4"
+                href="https://twitter.com/home"
+                target="blank"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -178,7 +186,11 @@ function Footer() {
                   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
                 </svg>
               </a>
-              <a className="mx-4" href="https://www.instagram.com/" target="blank">
+              <a
+                className="mx-4"
+                href="https://www.instagram.com/"
+                target="blank"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -204,7 +216,7 @@ function Footer() {
               </a>
             </div>
           </div>
-          <div className="copyright-footer text-center">
+          <div className="copyright-footer text-center pb-4">
             © 2023 apparat inc. All rights reserved
           </div>
         </div>
