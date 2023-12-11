0235068d8a8ce95e3f60038492472085daed9f71
helmdyck <helmuth.dyck@gmail.com>
2023-12-11 12:29:10 -0300

    Persist added

--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -1,16 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
-import cartReducer from "./cartSlice"
-import userReducer from "./userSlice"
-import pageReducer from "./pageSlice"
+import cartReducer from "./cartSlice";
+import userReducer from "./userSlice";
+import pageReducer from "./pageSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 
-const store = configureStore({
-  reducer: {
-    products: productReducer,
-    cart: cartReducer,
-    user: userReducer,
-    page: pageReducer,
-  },
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  user: userReducer,
+  page: pageReducer,
 });
 
-export default store;
+const persistConfig = { key: "root",  storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+const persistor = persistStore(store);
+
+
+
+export{store, persistor};
