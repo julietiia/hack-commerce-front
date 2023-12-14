c9fca9e350784a602843ce99967174f743017e33
helmdyck <helmuth.dyck@gmail.com>
2023-12-14 01:00:40 -0300

    My orders working an Footer CSS update

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,83 +1,88 @@
-import React, { useState } from "react";
+import "../components/css/MyOrders.css";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { clearCart } from "../redux/cartSlice";
+import { nanoid } from '@reduxjs/toolkit'
 import { Spinner } from "react-bootstrap";
 
 function MyOrders() {
   const userToken = useSelector((state) => state.user);
-  console.log(userToken)
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState([]);
 
-//   useEffect(() => {
-//     const getOrders = async () => {
-//       const response = await axios({
-//         method: "get",
-//         url: `${import.meta.env.VITE_PORT_URL}/panel/customers/${}`,
-//         headers: {
-//           Authorization: `Bearer ${adminToken.token}`,
-//         },
-//       });
-//       setOrders(response.data.orders);
-//     };
-//     getOrders();
-//   }, []);
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const response = await axios({
+          method: "get",
+          url: `${import.meta.env.VITE_PORT_URL}/orders/${userToken.id}`,
+          headers: {
+            Authorization: `Bearer ${userToken.token}`,
+          },
+        });
+        setOrders(response.data.orders);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getOrders();
+  }, []);
+
+  const calculateTotalPrice = (order) => {
+    return order.products.reduce((total, product) => {
+      return total + product.product.price * product.quantity;
+    }, 0);
+  };
 
   return (
     <>
-      {/* <div className="col-lg-6 col-md-12">
-        <div className="shopping-list-container">
-          <h3 className="ps-2">Your shopping list</h3>
-          <Breadcrumb />
+      <div className="container table-container">
+        <div className="row">
+          <div className="title">
+            <h3 className="mt-5">My orders</h3>
+          </div>
 
-          <div className="shopping-list">
-            {cartProducts.map((cart) => (
-              <div className="checkout-product" key={cart.product.id}>
-                <div className="d-flex">
-                  <div className="position-relative">
-                    <img
-                      className="checkout-product-image"
-                      src={`${import.meta.env.VITE_IMAGES_URL}/${
-                        cart.product.image[1]
-                      }`}
-                      alt={cart.product.name}
-                    />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">
-                      {cart.quantity}
-                    </span>
-                  </div>
+          <div className="col">
+            <div className="my-table">
+              <table className="table table-bordered my-table">
+                <thead>
+                  <tr>
+                    <th scope="col">Order</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Products</th>
+                    <th scope="col">State</th>
+                    <th scope="col">Total USD</th>
+                  </tr>
+                </thead>
+                {orders.map((order) => (
+                  <tbody key={order.id}>
+                    <tr>
+                      <td>{order.id}</td>
+                      <td>{order.date}</td>
 
-                  <div className="checkout-product-details ps-2">
-                    <p className="checkout-product-name">{cart.product.name}</p>
-                    <p className="checkout-product-ref">
-                      Ref.{cart.product.id}
-                    </p>
-                  </div>
-                </div>
-                <div className="">
-                  <p className="checkout-product-price">{cart.product.price}</p>
-                </div>
-              </div>
-            ))}
-
-            <hr />
-
-            <div className="total ps-2 pe-4">
-              <p className="checkout-total">Total</p>
-              <p className="checkout-total">
-                USD{" "}
-                {cartProducts.reduce(
-                  (acc, { product: { price }, quantity }) =>
-                    acc + (price ?? 0) * quantity,
-                  0
-                )}
-              </p>
+                      {order.products.map((product) => (
+                        <React.Fragment key={nanoid()}>
+                          <td className="d-flex">
+                            <p className="order-product-detail">
+                              Id.{product.product.id} - Qty.{product.quantity}
+                            </p>
+                            <p className="order-product-detail">
+                              - {product.product.name}
+                            </p>
+                          </td>
+                        </React.Fragment>
+                      ))}
+                      <td className="">{order.state}</td>
+                      <td className="fw-semibold">
+                        {calculateTotalPrice(order)}
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
+              </table>
             </div>
           </div>
-                    
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
