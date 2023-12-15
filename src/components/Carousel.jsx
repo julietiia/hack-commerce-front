1490211147a8b793e330146ec0ebd58e64a16bea
helmdyck <helmuth.dyck@gmail.com>
2023-12-15 01:06:12 -0300

    Caroussel responsive

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,19 +3,20 @@ import Carousel from "react-bootstrap/Carousel";
 import living_retro from "../assets/img/living_retro.jpg";
 import living_white from "../assets/img/living_white.jpg";
 import living_pink from "../assets/img/living_pink.jpg";
+import "../components/css/App.css"
 
 function HomeCarousel() {
   return (
     <Carousel>
-      <Carousel.Item>
+      <Carousel.Item className="carrousel-img">
         <img src={living_white} alt="" />
       </Carousel.Item>
 
-      <Carousel.Item>
+      <Carousel.Item className="carrousel-img">
         <img src={living_retro} alt="" />
       </Carousel.Item>
 
-      <Carousel.Item>
+      <Carousel.Item className="carrousel-img">
         <img src={living_pink} alt="" />
       </Carousel.Item>
     </Carousel>
